214e268712a28a4851a3e1e005905db984c6f676
Joshua Shaffer <joshua@joshuatshaffer.com>
2023-01-20 16:39:53 -0600

    wethsa

--- lib/pattern.spec.ts
+++ lib/pattern.spec.ts
@@ -1,12 +1,12 @@
 /// <reference types="jest-extended" />
 import { Expression } from "./dice-lang/dice-lang-ast";
-import { b, n, op } from "./pattern";
+import { b, InferBindings, n, op } from "./pattern";
 
 describe("pattern", () => {
   it("works", () => {
     const p = op(b("l"), "d", n(1));
     const e: Expression = { left: 45, operator: "d", right: 1 };
 
-    expect(p.match(e)).toEqual({ l: 45 });
+    expect(p.match(e)).toEqual<InferBindings<typeof p>>({ l: 45 });
   });
 });
--- lib/pattern.ts
+++ lib/pattern.ts
@@ -1,66 +1,71 @@
 import { BinaryOperation, Expression } from "./dice-lang/dice-lang-ast";
 
-type Bindings = Record<string, unknown>;
-
-interface Pattern<B extends Bindings> {
-  match: (x: Expression) => B | undefined;
-}
-
-export function b<N extends string>(name: N): Pattern<Record<N, Expression>>;
-
-export function b<N extends string, B extends Bindings>(
-  name: N,
-  p: Pattern<B>
-): Pattern<Record<N, Expression> & B>;
-
-export function b<N extends string, B extends Bindings>(
-  name: N,
-  p?: Pattern<B>
-): Pattern<Record<N, Expression> & (B | {})> {
-  return {
-    match: (x) => {
-      if (p) {
-        const m = p.match(x);
-        return m ? { ...m, [name]: x } : undefined;
-      }
-      return { [name as N]: x } as Record<N, Expression>;
-    },
-  };
-}
-
-export function n(equalTo?: number): Pattern<Record<never, unknown>> {
-  return {
-    match: (x) =>
-      typeof x === "number" && (equalTo === undefined || equalTo === x)
-        ? {}
-        : undefined,
-  };
-}
-
 type Simplify<T> = T extends unknown ? { [P in keyof T]: T[P] } & {} : never;
 
-export function op<L extends Bindings, R extends Bindings>(
-  l: Pattern<L>,
+type Bindings = Record<string, unknown>;
+
+class Pattern<I, O extends I, B extends Bindings> {
+  constructor(public readonly match: (i: I) => B | undefined) {}
+
+  test(i: I): i is O {
+    return this.match(i) !== undefined;
+  }
+}
+
+export type InferBindings<T extends Pattern<any, any, any>> = Simplify<
+  Exclude<ReturnType<T["match"]>, undefined>
+>;
+
+export function b<N extends string, I, O extends I>(
+  name: N
+): Pattern<I, O, Record<N, O>>;
+
+export function b<N extends string, I, O extends I, B extends Bindings>(
+  name: N,
+  p: Pattern<I, O, B>
+): Pattern<I, O, Record<N, O> & B>;
+
+export function b<N extends string, I, O extends I, B extends Bindings>(
+  name: N,
+  p?: Pattern<I, O, B>
+): Pattern<I, O, Record<N, O> & (B | {})> {
+  return new Pattern<I, O, Record<N, O> & (B | {})>((x) => {
+    if (p) {
+      const m = p.match(x);
+      return m ? ({ ...m, [name]: x } as Record<N, O> & B) : undefined;
+    }
+    return { [name as N]: x } as Record<N, O>;
+  });
+}
+
+export function n(equalTo?: number) {
+  return new Pattern<unknown, number, Record<never, unknown>>((x) =>
+    typeof x === "number" && (equalTo === undefined || equalTo === x)
+      ? {}
+      : undefined
+  );
+}
+
+export function op<LB extends Bindings, RB extends Bindings>(
+  l: Pattern<Expression, Expression, LB>,
   op: BinaryOperation["operator"],
-  r: Pattern<R>
-): Pattern<L & R> {
-  return {
-    match: (x) => {
-      if (typeof x === "number" || x.operator !== op) {
-        return undefined;
-      }
+  r: Pattern<Expression, Expression, RB>
+) {
+  return new Pattern<Expression, Expression, LB & RB>((x) => {
+    if (typeof x === "number" || x.operator !== op) {
+      return undefined;
+    }
 
-      const lm = l.match(x.left);
-      if (lm === undefined) {
-        return undefined;
-      }
+    const lm = l.match(x.left);
+    if (lm === undefined) {
+      return undefined;
+    }
 
-      const rm = r.match(x.right);
-      if (rm === undefined) {
-        return undefined;
-      }
+    const rm = r.match(x.right);
+    if (rm === undefined) {
+      return undefined;
+    }
 
-      return { ...lm, ...rm };
-    },
-  };
+    return { ...lm, ...rm };
+  });
 }
