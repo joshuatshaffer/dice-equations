bd7653024dfd9df5fd988855ed28cf4d6904cf67
Joshua Shaffer <joshua@joshuatshaffer.com>
2023-01-24 07:53:38 -0600

    p98yu

--- lib/math-helpers.ts
+++ lib/math-helpers.ts
@@ -10,10 +10,23 @@ export function reduceFraction(n: number, d: number) {
   return [n / f, d / f] as const;
 }
 
+/**
+ * Product of `n*(n-1)*(n-2)*...*2*1`.
+ *
+ * Non-integer values of n are rounded down, i.e. `factorial(n) === factorial(Math.floor(n))`
+ */
 export function factorial(n: number): number {
+  if (n < 0) {
+    return NaN;
+  }
+
   let a = 1;
-  for (let i = n; i > 0; --i) {
+  for (let i = 2; i <= n; ++i) {
     a *= i;
   }
   return a;
+}
+
+export function choose(n: number, k: number): number {
+  return factorial(n) / (factorial(k) * factorial(n - k));
 }
