2cfee126b184c3c895462bb78ae95049e027c920
Joshua Shaffer <joshua@joshuatshaffer.com>
2023-01-24 08:01:56 -0600

    asdf

--- lib/dice-lang/dice-lang-parse.spec.ts
+++ lib/dice-lang/dice-lang-parse.spec.ts
@@ -1,17 +1,15 @@
-import P from "parsimmon";
-import { Expression } from "./dice-lang-ast";
+import { Program } from "./dice-lang-ast";
 import { diceParser } from "./dice-lang-parse";
 
 describe("diceParser", () => {
-  it("works", () => {
-    expect(diceParser.parse("2d5")).toEqual<P.Success<Expression>>({
-      status: true,
-      value: {
+  it("parses 2d5", () => {
+    expect(diceParser.tryParse("2d5")).toEqual<Program>([
+      {
         type: "BinaryOperation",
         left: { type: "NumberLiteral", value: 2 },
         operator: "d",
         right: { type: "NumberLiteral", value: 5 },
       },
-    });
+    ]);
   });
 });
