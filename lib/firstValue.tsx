a47b76fa9acee5aaf1d9bc90b24e1fa4b3540b58
Joshua Shaffer <joshua@joshuatshaffer.com>
2023-12-31 10:58:31 -0600

    Remove junk code.

--- lib/firstValue.tsx
+++
@@ -1,7 +0,0 @@
-// the largest value k such that for all x in xs, x = k*n+min(xs)
-export function firstValue(
-  x: string | string[] | undefined
-): string | undefined {
-  if (Array.isArray(x)) return x[0];
-  return x;
-}
--- lib/foo.py
+++
@@ -1,9 +0,0 @@
-import math
-
-
-def choose(n, k):
-    return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
-
-
-def dice(n, s):
-    return lambda x: (1 / math.factorial(n-1)) * sum([(-1)**k * choose(n, k) * ((x/s) - k)**(n - 1) for k in range(0, math.floor(x/s) + 1)])
